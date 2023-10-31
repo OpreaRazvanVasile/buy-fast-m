3d2372c3756c58e3a07bdb3f8b4ac08c865e0625
OpreaRazvanVasile <razvanvasile20022@gmail.com>
2023-10-31 15:08:21 +0200

    stripe

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import { BrowserRouter } from 'react-router-dom'
 import { UserProvider } from './contexts/user-context/user.context';
 import { CategoriesProvider } from './contexts/categories-context/categories.contex';
 import {CartProvaider} from './contexts/cart-context/cart-context';
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripePromise } from './utils/fierbase/stripe/stripe.utils';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -16,7 +18,11 @@ root.render(
     <UserProvider>
     <CategoriesProvider>
         <CartProvaider>
+          <Elements stripe={loadStripePromise}>
+       
         <App />
+        </Elements>
+     
 
         </CartProvaider>
     
